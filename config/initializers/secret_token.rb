7c803e2d68d5c3b7f589065e8f1390d9662ab3cd
chiastolite <chiastolite.1980@gmail.com>
2013-02-19 21:29:33 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestForOpsworks::Application.config.secret_token = 'a4183afc53b1e4de395b01727ff680abe78b2eb1ed5077ec515579569d1f8c69f07a6a8b2d4cef7a332df1e3c5b24d2c96ac9b11f2f8e1c0005284d992b3b143'
